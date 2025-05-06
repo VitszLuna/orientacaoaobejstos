b5466ac0ca9a10964b8f54718c0bb4a6ab250b4b
Vitoria Luna! <isabeli.vitoria.luna@gmail.com>
2025-05-05 22:09:18 -0300

    Update exemplo.js

--- exemplo.js
+++ exemplo.js
@@ -1,16 +1,26 @@
+// Função 1: Mostra informações da fruta
 function mostrarFruta() {
   const nome = "Maçã";
   const cor = "Vermelha";
   const sabor = "Doce";
+
+  return `Nome: ${nome}, Cor: ${cor}, Sabor: ${sabor}`;
 }
-alert("Informações da Fruta: nome:Maçã cor:Vermelha sabor:Doce");
+
+// Adicionando evento de clique no botão para mostrar o alerta
+document.getElementById("botao1").addEventListener("click", function () {
+  alert(mostrarFruta());
+});
 
 // Função 2: Mostra informações do teclado
 function mostrarTeclado() {
   const qualidade = "Novo";
   const disponibilidade = "Disponível";
 
-  alert(
-    "Informações do Teclado:Qualidade:Nova, Disponibilidade:disponivel"
-  );
+  alert(`Informações do Teclado: Qualidade: ${qualidade}, Disponibilidade: ${disponibilidade}`);
 }
+
+// Adicionando evento de clique no botão do teclado
+document.getElementById("botao2").addEventListener("click", function () {
+  mostrarTeclado();
+});
